2aad01d96198d318657389c8fc2ce4a6de2662e0
Hung-Lin, Chen <hunglin59638@gmail.com>
2023-07-13 00:47:13 +0800

    REIC-8 build db

--- backend/db.py
+++ backend/db.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import os
+import json
 import hashlib
 import requests
 import subprocess
-import marisa_trie
+import tarfile
 import tempfile
-import cython
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from backend.common import set_threads, set_out_dir, open_gz
-from backend.taxonparse import get_lineage
+from backend.taxonparse import list_subtree
 
 
 def validate_md5(file, md5):
@@ -33,25 +34,40 @@ def download_file(url, out_dir, rewrite=False):
     out_dir: path to output directory -> Path
     return: (returncode, out_file)
     """
+    if url.startswith("https://"):
+        method = "https"
+    elif url.startswith("rsync://"):
+        method = "rsync"
+    else:
+        raise Exception(f"Unknown download method: {url}")
+
     out_dir = Path(out_dir)
     out_dir.mkdir(parents=True, exist_ok=True)
     out_file = out_dir / Path(url).name
     if out_file.is_file() and not rewrite:
         print(f"{out_file} already exists")
-        retruncode = 0
+        returncode = 0
     else:
         try:
-            with requests.get(url, stream=True) as r, open(out_file, "wb") as f:
-                r.raise_for_status()
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-            retruncode = 0
+            if method == "https":
+                with requests.get(url, stream=True) as r, open(out_file, "wb") as f:
+                    r.raise_for_status()
+                    for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
+                returncode = 0
+            elif method == "rsync":
+                returncode = os.system(
+                    f"rsync --copy-links --times --quiet {url} {out_dir}"
+                )
+            else:
+                returncode = 1
+
         except Exception as e:
             print(f"Failed to download {url}")
             print(e)
-            retruncode = 1
+            returncode = 1
 
-    return (retruncode, out_file)
+    return (returncode, out_file)
 
 
 @set_out_dir
@@ -83,29 +99,9 @@ def build_acc2taxid(acc2taxid=None, out_dir=None):
     return marisa_file
 
 
-@set_out_dir
-def build_nt_db(out_dir=None, rewrite=False):
-    fasta_url = "https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nt.gz"
-    print("Start downloading nt fasta file")
-    returncode, fasta = download_file(url=fasta_url, out_dir=out_dir, rewrite=rewrite)
-    if returncode:
-        raise Exception("Failed to download nt fasta file")
-
-    md5_url = "https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nt.gz.md5"
-    returncode, md5_file = download_file(url=md5_url, out_dir=out_dir)
-    if returncode:
-        raise Exception("Failed to download nt md5 file")
-    print("Start validating nt fasta file")
-    if validate_md5(file=fasta, md5=md5_file.read_text().split(" ")[0]):
-        print("nt fasta file is valid")
-        return fasta
-    else:
-        raise Exception("nt fasta file is invalid")
-
-
 @set_threads
 def convert_blastdb_to_fasta(
-    blastdb, basename, taxids=[], min_len=1, compress=True, threads=0
+    blastdb, basename, taxids=[], min_len=1, compress=False, threads=0
 ):
     """
     Convert blastdb to fasta file
@@ -115,24 +111,26 @@ def convert_blastdb_to_fasta(
     """
     taxids = [str(taxid) for taxid in taxids]
     basename = Path(basename)
-    fasta_file = basename.parent / f"{basename.name}.fna"
-    split_str = "----"
+    out_dir = basename.parent
+    fasta_fpath = basename.parent / f"{basename.name}.fa"
+    delim = ">>>"
     extract_cmd = [
         "blastdbcmd",
         "-db",
         blastdb,
         "-outfmt",
-        f"%a{split_str}%T{split_str}%s",
+        f"%a{delim}%T{delim}%s{delim}%l",
     ]
-    taxid_check = dict([(taxid, True) for taxid in taxids])
     if taxids:
-        extract_cmd.extend(["-taxids", ",".join(taxids)])
+        taxidlist = out_dir / f"{basename.name}_taxidlist.txt"
+        taxidlist.write_text("\n".join(taxids))
+        extract_cmd.extend(["-taxidlist", taxidlist, "-target_only"])
     else:
         extract_cmd.extend(["-entry", "all"])
 
     min_len = 0 if min_len is None else min_len
 
-    with open(fasta_file, "w") as f:
+    with open(fasta_fpath, "w") as f:
         blastdbcmd_proc = subprocess.Popen(extract_cmd, stdout=subprocess.PIPE)
 
         while True:
@@ -140,135 +138,96 @@ def convert_blastdb_to_fasta(
             if not line:
                 break
 
-            seqid, taxid, seq = line.strip(" \n").split(split_str)
-            if not taxid_check.get(taxid, False):
-                continue
-            if len(seq) <= min_len:
-                continue
+            seqid, taxid, seq, seqlen = line.split(delim)
 
-            f.write(f">{seqid}|{taxid}\n{seq}\n")
+            if int(seqlen) >= min_len:
+                f.write(f">{seqid}|{taxid}\n{seq}\n")
 
     if compress:
-        compress_proc = subprocess.run(["pigz", "-p", threads, fasta_file])
+        compress_proc = subprocess.run(["pigz", "-p", threads, fasta_fpath])
         if compress_proc.returncode:
             raise Exception("Failed to compress fasta file")
-        fasta_file = fasta_file.with_suffix(".fna.gz")
+        fasta_fpath = fasta_fpath.with_suffix(".fa.gz")
 
-    return fasta_file
+    return fasta_fpath
 
 
 @set_out_dir
-def split_nt(fasta, acc2taxid_marisa, taxdump_dir, out_dir=None, min_len=50):
-    """
-    Split nt fasta to human and non-human
-    fasta: path to nt fasta -> Path
-    out_dir: path to output directory -> Path
-    """
-    stat = {
-        "total_n": 0,
-        "human_n": 0,
-        "non_human_n": 0,
-        "removed": {
-            "no_taxid": {"n": 0, "seqids": []},
-            "less_than": {"cutoff": min_len, "n": 0, "seqids": []},
-            "black_taxa": {"n": 0, "seqids": []},
-        },
-    }
+def build_blastdb(taxdump_dir, db_type="nt", out_dir=None, resume=False):
+    def batch_get_blastdb(url, out_dir):
+        tar_fpath = out_dir / Path(url).name
+        md5_fpath = tar_fpath.with_suffix(".gz.md5")
 
-    acc2taxid_trie = Acc2Taxid(acc2taxid_marisa)
-
-    taxids = set()
-    c = 0
-    removed = []
-    positions = {}
-    last_pos = 0
-    with open_gz(fasta) as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            offset = f.tell()
-            line_len = offset - last_pos
-            if line[0] == ">":
-                seqid = line.strip(" >\n").split(".")[0]
-                taxid = acc2taxid_trie.search_taxid(seqid)
-                taxid = str(taxid) if taxid is not None else taxid
-                if taxid:
-                    taxids.add(taxid)
-                else:
-                    removed.append(seqid)
-                    seqid = None
-                positions[seqid] = {
-                    "header": {"fr": last_pos, "len": line_len},
-                    "seq": {"fr": last_pos + line_len + 1, "len": 0, "seqlen": 0},
-                }
-                c += 1
-                if c % 1000000 == 0:
-                    print(f"Processed {c} sequences")
-            else:
-                if seqid is None:
-                    continue
-                seqid_end = (
-                    positions[seqid]["header"]["fr"] + positions[seqid]["header"]["len"]
-                )
-                positions[seqid]["seq"]["len"] = offset - seqid_end
-                positions[seqid]["seq"]["seqlen"] += line_len - 1
-
-            last_pos = offset
-
-    stat["total_n"] = c
-    print(f"Total number of sequences: {c}")
-
-    taxid2lineage = get_lineage(taxids, taxdump_dir)
-
-    conditions = {}
-    for taxid, lineage in taxid2lineage.items():
-        if lineage.get("species", {}).get("taxid", "") == "9606":
-            conditions[taxid] = 0
-        elif lineage.get("kingdom", {}).get("taxid", "") in ("33090", "33208"):
-            conditions[taxid] = 1
+        rewrite = False
+        if tar_fpath.is_file() and md5_fpath.is_file():
+            rewrite = not validate_md5(tar_fpath, md5_fpath.read_text().split(" ")[0])
+            print(f"{tar_fpath} exists, and it is valid")
         else:
-            conditions[taxid] = 2
+            print("Downloding", url)
+            returncode, tar_fpath = download_file(url, out_dir, rewrite=rewrite)
+            if returncode:
+                return (returncode, url)
+            md5_url = url + ".md5"
+            returncode, md5_fpath = download_file(md5_url, out_dir)
+            if returncode:
+                return (returncode, url)
+            md5 = md5_fpath.read_text().split(" ")[0]
+            print("Validating", tar_fpath)
+            if not validate_md5(tar_fpath, md5):
+                print("MD5 validation failed")
+                return (1, url)
+            print("Extracting", tar_fpath)
+            with tarfile.open(tar_fpath, "r:gz") as tar:
+                tar.extractall(out_dir)
+        print("Done", tar_fpath)
+        return (0, url)
 
-    human_fasta = out_dir / "human.fa"
-    nhuman_fasta = out_dir / "non_human.fa"
-    c = 0
-    con = 1
-    with open_gz(fasta, "rt") as f, open(human_fasta, "w") as human_f, open(
-        nhuman_fasta, "w"
-    ) as nhuman_f:
-        for seqid, offset_rec in positions.items():
-            c += 1
-            taxid = offset_rec["taxid"]
-            if taxid is None:
-                stat["removed"]["no_taxid"]["n"] += 1
-                stat["removed"]["no_taxid"]["seqids"].append(seqid)
-                continue
-            elif offset_rec["seq"]["seqlen"] < min_len:
-                stat["removed"]["less_than"]["n"] += 1
-                stat["removed"]["less_than"]["seqids"].append(seqid)
-                continue
+    human_fa = out_dir / "non_human_{db_type}.fa"
+    non_human_fa = out_dir / "human_{db_type}.fa"
 
-            con = conditions.get(taxid, 2)
-            f.seek(offset_rec["header"]["fr"])
-            rec_len = offset_rec["header"]["len"] + offset_rec["seq"]["len"]
-            if con == 0:
-                human_f.write(f.read(rec_len))
-                stat["human_n"] += 1
-            elif con == 1:
-                stat["removed"]["black_taxa"]["seqids"].append(seqid)
-            elif con == 2:
-                nhuman_f.write(f.read(rec_len))
-                stat["non_human_n"] += 1
+    seq_type = "nucl" if db_type == "nt" else "prot"
+    metadata_json_url = (
+        f"https://ftp.ncbi.nlm.nih.gov/blast/db/{db_type}-{seq_type}-metadata.json"
+    )
+    returncode, metadata_json = download_file(url=metadata_json_url, out_dir=out_dir)
+    assert returncode == 0, "Failed to download nt metadata"
 
-            if c % 1000000 == 0:
-                break
+    metadata = json.loads(metadata_json.read_text())
+    urls = [url.replace("ftp://", "https://") for url in metadata["files"]]
+    blastdb_dir = out_dir / "blastdb"
+    args = [(url, blastdb_dir) for url in urls]
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(batch_get_blastdb, *arg) for arg in args]
+        results = [future.result() for future in as_completed(futures)]
+    if any([result[0] for result in results]):
+        raise Exception("Failed to download blastdb")
+    db_basename = blastdb_dir / db_type
 
-    return stat
+    all_taxids = list_subtree(taxids=[131567], taxdump_dir=taxdump_dir)
+    excluded_taxids = list_subtree(taxids=[33090, 33208], taxdump_dir=taxdump_dir)
+    human_taxids = list_subtree(taxids=[9606], taxdump_dir=taxdump_dir)
+    non_human_taxids = all_taxids - human_taxids - excluded_taxids
+
+    db_args = [
+        (db_basename, non_human_fa.with_suffix(), non_human_taxids, 50, False),
+        (db_basename, human_fa.with_suffix(), human_taxids, 50, False),
+    ]
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        futures = [executor.submit(convert_blastdb_to_fasta, *arg) for arg in db_args]
+        results = [future.result() for future in as_completed(futures)]
+
+    if any([result[0] for result in results]):
+        raise Exception("Failed to convert blastdb to fasta")
+
+    return {
+        "blastdb": db_basename,
+        "fasta": {"human": {human_fa}, "non_human": non_human_fa},
+    }
 
 
 @set_threads
-def build_human_db(human_nt, out_dir, threads=0):
+@set_out_dir
+def build_human_db(human_fa, out_dir, threads=0):
     """
     Build human db for bowtie2 and HISAT2
     human_nt: path to human nt fasta -> Path
@@ -277,34 +236,33 @@ def build_human_db(human_nt, out_dir, threads=0):
 
     """
     bowtie2_dir = out_dir / "bowtie2"
-    hisat2_dir = out_dir / "hisat2"
     bowtie2_dir.mkdir(parents=True, exist_ok=True)
-    hisat2_dir.mkdir(parents=True, exist_ok=True)
 
-    # nt_human_fasta = convert_blastdb_to_fasta(
-    #     blastdb=blastdb_nt,
-    #     basename=bowtie2_dir / "nt_human",
-    #     threads=threads,
-    #     compress=False,
-    #     taxids=["9606"],
-    # )
-    bowtie_base_index = bowtie2_dir / "human_nt"
+    bowtie_base_index = bowtie2_dir / "human"
     bowtie_index_p = subprocess.Popen(
-        ["bowtie2-build", "--threads", threads, human_nt, bowtie_base_index]
+        ["bowtie2-build", "--threads", threads, human_fa, bowtie_base_index]
     )
 
     hisat2_url = "https://genome-idx.s3.amazonaws.com/hisat/grch38_tran.tar.gz"
-    returncode, hisat2_fname = download_file(url=hisat2_url, out_dir=out_dir)
+    returncode, hisat2_tar = download_file(url=hisat2_url, out_dir=out_dir)
     if returncode:
         raise Exception("Failed to download hisat2 index")
-    tar_proc = subprocess.run(["tar", "-xf", hisat2_fname, "-C", hisat2_dir])
+
+    tar_dirname = None
+    with tarfile.open(hisat2_tar, "r:gz") as tar:
+        for tarinfo in tar:
+            if tarinfo.isfile() and tarinfo.name.endswith(".ht2"):
+                tar.extract(tarinfo, out_dir)
+            elif tarinfo.isdir():
+                tar_dirname = tarinfo.name
+    hisat2_dir = out_dir / tar_dirname
+    hisat2_dir = hisat2_dir.rename(out_dir / f"{tar_dirname}_hisat")
+    hisat2_tar.unlink()
     bowtie_index_p.wait()
-    if tar_proc.returncode:
-        raise Exception("Failed to extract hisat2 index")
 
     return {
         "bowtie2": bowtie_base_index,
-        "hisat2": hisat2_fname.name.replace(".tar.gz", "") + "genome_tran",
+        "hisat2": hisat2_dir / "genome_tran",
     }
 
 
@@ -341,14 +299,28 @@ def build_human_hisat2_index(out_dir, threads=0):
     )
 
 
-class Acc2Taxid:
-    def __init__(self, acc2taxid):
-        """
-        acc2taxid: path to acc2taxid marisa trie -> Path
-        """
-        self.trie = marisa_trie.RecordTrie("L").mmap(str(acc2taxid))
+@set_threads
+@set_out_dir
+def main(out_dir=None, threads=0):
+    taxdump_dir = out_dir / "taxdump"
+    taxdump_dir.mkdir(parents=True, exist_ok=True)
+    if len([f.is_file() for f in taxdump_dir.glob("*dmp")]) > 0:
+        print("Taxdump already exists")
+    else:
+        returncdoe, tardump_tar = download_file(
+            url="https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz",
+            out_dir=out_dir,
+        )
+        if returncdoe:
+            raise Exception("Failed to download taxdump")
+        with tarfile.open(tardump_tar, "r:gz") as tar:
+            tar.extractall(taxdump_dir)
+        tardump_tar.unlink()
 
-    def search_taxid(self, acc):
-        match = self.trie.get(acc)
-        if match:
-            return match[0][0]
+    blastdbs = build_blastdb(out_dir=out_dir, db_type="nt")
+
+    human_dbs = build_human_db(
+        human_fa=blastdbs["fasta"]["human"], out_dir=out_dir, threads=0
+    )
+
+    return [blastdbs, human_dbs]
